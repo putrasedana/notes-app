e378d400366df2231258d3ddb9b7d613a320eb50
Putra Sedana <putrasedana03@gmail.com>
2024-07-30 09:16:12 +0800

    menambahkan dependencies ke folder dist

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import "./styles/style.css";
 import "./script/components/index.js";
 import home from "./script/view/home.js";
 import "toastr/build/toastr.min.css";
+import gsap from "gsap";
+import Swal from "sweetalert2";
+import toastr from "toastr";
 
 document.addEventListener("DOMContentLoaded", () => {
   home();
